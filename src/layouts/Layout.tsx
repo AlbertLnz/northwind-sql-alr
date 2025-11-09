1c72970582eb9fae090c216823e5662c22779b36
Albert Lanza <albert2000_lanza@hotmail.com>
2025-11-09 11:28:18 +0100

    Mobile view disabled

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -8,18 +8,24 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <main className='min-h-screen max-w-[75%] p-10 mx-auto'>
-      <h1 className='text-4xl font-extrabold pb-10 text-[#221d1d]'>
-        Northwind SQL Practice{' '}
-        <span className='text-2xl text-[#8f8e8e]'>by Albert Lanza</span>
-      </h1>
-      <Header />
-      <QueryPractice />
-      <section className='flex flex-col gap-y-8 max-w-screen'>
-        {children}
-      </section>
-      <Footer />
-    </main>
+    <>
+      <p className='md:hidden flex text-center absolute top-1/2 -translate-y-1/2 text-lg px-8'>
+        Esta web está diseñada actualmente para dispositivos más grandes, se
+        está trabajando para adaptarla a dispositivos más pequeños.
+      </p>
+      <main className='min-h-screen max-w-[75%] p-10 mx-auto hidden md:block'>
+        <h1 className='text-4xl font-extrabold pb-10 text-[#221d1d]'>
+          Northwind SQL Practice{' '}
+          <span className='text-2xl text-[#8f8e8e]'>by Albert Lanza</span>
+        </h1>
+        <Header />
+        <QueryPractice />
+        <section className='flex flex-col gap-y-8 max-w-screen'>
+          {children}
+        </section>
+        <Footer />
+      </main>
+    </>
   )
 }
 
